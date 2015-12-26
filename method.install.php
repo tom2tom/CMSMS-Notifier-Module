f8413d8cafb9a039be755670c6956765393fef3c
tom2tom <tpgww@onepost.net>
2015-12-26 11:51:57 +1100

    updated suggested code for password management

--- method.install.php
+++ method.install.php
@@ -28,15 +28,33 @@ $this->SetPreference('privaccess','9eLpsFmdA9BTD7YkP6TWL5z0VRXMZfNRF/YRWFm+dcpe0
 $this->SetPreference('privapi','E+HXtNA+Zzgv+bC0m28ySJ999zNN/Qht/0kKOY1Ma4R+25fFuABXNRBta3ehEiJC520w2U+Az32oZO0Zk06c8WFeM/ajUZZBMqikLu50Uj5S7yAJehB1Nfh7go6CTIsXSNKxRn8MQCkbqN5VLgKyWfWn0M//mY6l1Oq1X05GahGPxQeSXrwwyDpT582ITcjqGWsjtaNmXOtUeX+ufCHdmPjjhl+CNw0hOyDf84u4kGt9kVbHz6CMxWOC+Lsv9YrVXsAIXJeP3PH3adMr7D5S9zSU6CZbI9R+Rg6Ti2TxtNy1DpQ8VIjqa1BUW7j6ngqu9XQk5YTIaPU=');
 
 /* TODO ADMIN UI - support password change
+$lang[] = 'Pass-phrase for securing sensitive data';
+
 	$pw = $this->GetPreference('masterpass');
 	if($pw)
 		$pw = Utils::unfusc($pw);
 	$smarty->assign('masterpass',$pw);
+OR
+	$smarty->assign('masterpass',
+		$this->CreateTextArea(false,$id,$pw,'masterpass',
+		'cloakpw',$id.'passwd','','',50,3,'','','style="height:3em;"'));
+
+	$jsincs[] = '<script type="text/javascript" src="'.$baseurl.'/include/jquery.inputcloak.min.js"></script>';
+	$jsloads[] =<<<EOS
+ $('#{$id}passwd').inputCloak({
+  type:'see4',
+  symbol:'\u2022'
+ });
+
 TEMPLATE
  <p class="pagetext">{$mod->Lang('prompt_master_password')}:</p>
  <p class="pageinput">
-  <input type="password" name="{$actionid}masterpass" value="{$masterpass}" size="36" maxlength="96" />
+ {$masterpass}
+ OR
+ <textarea id="m1_passwd" class="cloakpw" style="height:3em;" rows="3" cols="50"
+	name="m1_masterpass">{$masterpass}</textarea>
  </p>
+ 
 SAVING
 	$oldpw = $this->GetPreference('masterpass');
 	if($oldpw)
