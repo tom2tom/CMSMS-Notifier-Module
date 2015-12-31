ee79f383329d3bd3de5e1806c270ca1a21f9e89e
tom2tom <tpgww@onepost.net>
2015-12-31 11:52:43 +1100

    support direct-return for secure sites

--- action.twitaccount.php
+++ action.twitaccount.php
@@ -15,13 +15,19 @@ try
 		try
 		{
 			$conn = new TwitterCredential($apipublic,$apiprivate);
-			//when returning from twitter, MUST redirect with a valid $returnid, so
-			//we don't return here directly
-			$url = $this->CreateLink($id,'default',NULL,NULL,array('returnid'=>$returnid),NULL,TRUE);
-			//cleanup & force a secure return-communication (which, if the site is non-secure,
-			//freaks the browser first-time, AND stuffs up on-page-include-URLs, requiring
-			//a local redirect to fix)
-			$callback = str_replace(array($config['root_url'],'amp;'),array($config['ssl_url'],''),$url);
+			if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on')
+			{
+				//cleanup & force a secure return-communication, which freaks the browser
+				//first-time, AND stuffs up on-page-include-URLs, requiring a local redirect
+				//(in this case from action.twitback) to fix
+				$url = $this->CreateLink($id,'twitback',NULL,NULL,array(),NULL,TRUE);
+				$callback = str_replace(array($config['root_url'],'amp;'),array($config['ssl_url'],''),$url);
+			}
+			else
+			{
+				$url = $this->CreateLink($id,'twitaccount',NULL,NULL,array(),NULL,TRUE);
+				$callback = str_replace('amp;','',$url);
+			}
 			$message = $conn->gogetToken($callback); //should redirect to get token
 			//if we're still here, an error occurred
 		}
@@ -30,19 +36,30 @@ try
 			$message = $e->getMessage();
 		}
 	}
-	elseif(isset($params['oauth_verifier'])) //authorisation completed
+	elseif(isset($_REQUEST['oauth_token']) || isset($params['oauth_token'])) //authorisation was completed
 	{
+		if (isset($_REQUEST['oauth_token']))
+		{
+			$returnid = ''; //not set on direct-return from twitter
+			$token = $_REQUEST['oauth_token'];
+			$verf = $_REQUEST['oauth_verifier'];
+		}
+		else
+		{
+			$token = $params['oauth_token'];
+			$verf = $params['oauth_verifier'];
+		}
 		list($apipublic,$apiprivate) = $twt->ModuleAppTokens();
 		try
 		{
-			$conn = new TwitterCredential($apipublic,$apiprivate,$params['oauth_token'],NULL);
+			$conn = new TwitterCredential($apipublic,$apiprivate,$token,NULL);
 			//seek enduring credentials
-			$token = $conn->getAuthority($params['oauth_verifier']);
+			$token = $conn->getAuthority($verf);
 			if(is_array($token))
 			{
 				if($twt->SaveTokens($token['screen_name'],
 						$token['oauth_token'],$token['oauth_token_secret']))
-					$message = $this->Lang('status_complete');
+					$message = $this->Lang('status_complete',$token['screen_name']);
 				else
 					$message = $this->Lang('err_data_type',$this->Lang('err_token'));
 			}
@@ -54,7 +71,8 @@ try
 			$message = $e->getMessage();
 		}
 	}
-
+	else
+		exit;
 }
 catch (NoHelperException $e)
 {
