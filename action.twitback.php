21d58bbf282b975787e6ccf2de5b2a66e46cdc0c
tom2tom <tpgww@onepost.net>
2015-12-31 11:46:50 +1100

    rename action

---
+++ action.twitback.php
@@ -0,0 +1,18 @@
+<?php
+#----------------------------------------------------------------------
+# Module: Notifier - a communications module
+# Action: twitback - after return from Twitter, redirect to cleanup fake-secure URLs and finish establishing credentials
+#----------------------------------------------------------------------
+# See file Notifier.module.php for full details of copyright, licence, etc.
+#----------------------------------------------------------------------
+
+if(isset($_REQUEST['oauth_token'])) //authorisation was completed
+{
+	//preserve values across redirection
+	$params['oauth_token'] = $_REQUEST['oauth_token'];
+	$params['oauth_verifier'] = $_REQUEST['oauth_verifier'];
+//	$this->Redirect($id,'twitaccount','',$params);
+}
+$this->Redirect($id,'twitaccount','',$params);
+//exit;
+?>
