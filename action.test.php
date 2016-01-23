c65eab4172b316a9db74370badd0ba4f06ee08f3
tom2tom <tpgww@onepost.net>
2016-01-24 08:01:54 +1100

    conform revised address-validator API

--- action.test.php
+++ action.test.php
@@ -13,29 +13,30 @@ $to = $params['address'];
 if(!$to)
 	$this->Redirect($id,'defaultadmin',$returnid,array('activetab'=>'test'));
 
-$pattern = (isset($params['smspattern'])) ? $params['smspattern']:$this->GetPreference('smspattern');
-
 $funcs = new MessageSender();
-$res = $funcs->ValidateAddress($to,$pattern);
-switch($res)
+$clean = $funcs->ValidateAddress($to,$pattern);
+if($clean['text']) //valid phone
 {
- case 1: //valid phone
-	$prefix = (isset($params['smsprefix'])) ? $params['smsprefix']:$this->GetPreference('smsprefix');
+	$prefix = (!empty($params['smsprefix'])) ? $params['smsprefix']:$this->GetPreference('smsprefix');
+	$pattern = (!empty($params['smspattern'])) ? $params['smspattern']:$this->GetPreference('smspattern');
 	$body = 'The CMSMS Notifier module sent this test-message';
 	$parms = array('prefix'=>$prefix,'pattern'=>$pattern,'body'=>$body);
-	list($res,$msg) = $funcs->Send(FALSE,$to,$parms,FALSE,FALSE);
-	break;
- case 2: //valid email
+	list($res,$msg) = $funcs->Send(FALSE,$clean['text'],$parms,FALSE,FALSE);
+}
+elseif($clean['email']) //valid email
+{
 	$body = 'The CMSMS Notifier module sent this test-message';
 	$parms = array('subject'=>'Test message arrived','body'=>$body);
-	list($res,$msg) = $funcs->Send(FALSE,$to,FALSE,$parms,FALSE);
-	break;
- case 3: //valid handle
+	list($res,$msg) = $funcs->Send(FALSE,$clean['email'],FALSE,$parms,FALSE);
+}
+elseif($clean['tweet']) //valid handle
+{
 	$body = 'This tweet was posted by the CMSMS Notifier module as a test';
 	$parms = array('body'=>$body);
-	list($res,$msg) = $funcs->Send(FALSE,$to,FALSE,FALSE,$parms);
-	break;
- default:
+	list($res,$msg) = $funcs->Send(FALSE,$clean['tweet'],FALSE,FALSE,$parms);
+}
+else
+{
 	list($res,$msg) = array(FALSE,$this->Lang('err_address'));
 }
 
